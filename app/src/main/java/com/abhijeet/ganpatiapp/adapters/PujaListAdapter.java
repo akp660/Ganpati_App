6c655ebeb1ea0a7db4c7c4f183cf8e0b82bf9c65
abhijeet <abhijeetpandey.dhn@gmail.com>
2024-09-18 01:19:51 +0530

    Updated aarti and kundali layout and added on click listner

--- app/src/main/java/com/abhijeet/ganpatiapp/adapters/PujaListAdapter.java
+++ app/src/main/java/com/abhijeet/ganpatiapp/adapters/PujaListAdapter.java
@@ -1,6 +1,13 @@
 package com.abhijeet.ganpatiapp.adapters;
 
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -39,9 +46,17 @@ public class PujaListAdapter extends RecyclerView.Adapter<PujaListAdapter.ViewHo
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Context context = view.getContext();
+
+                vibrate(context, 50);
+
                 Intent intent = new Intent(view.getContext(), puja_list_view.class);
                 intent.putExtra("name",name);
                 view.getContext().startActivity(intent);
+
+                if (context instanceof Activity) {
+                    ((Activity) context).overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                }
             }
         });
     }
@@ -64,6 +79,19 @@ public class PujaListAdapter extends RecyclerView.Adapter<PujaListAdapter.ViewHo
 
         public void setData(String name) {
             Name.setText(name);
+        }
+    }
+
+    // Vibrate while selecting the Aarti
+    public static void vibrate(Context context, long milliseconds) {
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        if (vibrator != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibrator.vibrate(VibrationEffect.createOneShot(milliseconds, VibrationEffect.DEFAULT_AMPLITUDE));
+            } else {
+                // Deprecated in API 26 (Oreo) but still works for earlier versions
+                vibrator.vibrate(milliseconds);
+            }
         }
     }
 }
