4302c5f3757137f49f88950bbe8b5afa2e58622a
abhijeet <abhijeetpandey.dhn@gmail.com>
2024-09-23 22:41:20 +0530

    Updated aarti and kundali layout and added on click listner

--- app/src/main/java/com/abhijeet/ganpatiapp/activities/booking.java
+++ app/src/main/java/com/abhijeet/ganpatiapp/activities/booking.java
@@ -1,5 +1,6 @@
 package com.abhijeet.ganpatiapp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -23,4 +24,18 @@ public class booking extends AppCompatActivity {
             return insets;
         });
     }
+
+
+    // Override the onBackPressed to handle the back button press
+    @Override
+    public void onBackPressed() {
+        // Handle the back press like you would in the back button click
+        super.onBackPressed();
+        Intent intent = new Intent(booking.this, HomePageVer2Activity.class);
+        startActivity(intent);
+
+        // Trigger the slide-in animation
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
 }
