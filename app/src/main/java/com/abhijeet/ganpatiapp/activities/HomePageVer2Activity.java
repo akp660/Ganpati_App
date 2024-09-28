a6a3b98fd82e93b8ddbf1d34d680bf9d54d4bb11
abhijeet <abhijeetpandey.dhn@gmail.com>
2024-09-29 00:25:50 +0530

    updated

--- app/src/main/java/com/abhijeet/ganpatiapp/activities/HomePageVer2Activity.java
+++ app/src/main/java/com/abhijeet/ganpatiapp/activities/HomePageVer2Activity.java
@@ -1,7 +1,7 @@
 package com.abhijeet.ganpatiapp.activities;
-
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.VibrationEffect;
@@ -10,19 +10,20 @@ import android.transition.AutoTransition;
 import android.transition.TransitionManager;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 import com.abhijeet.ganpatiapp.R;
 
 public class HomePageVer2Activity extends AppCompatActivity {
 
     LinearLayout layout, detailsText, app_info, app_infp_detail;
-    CardView kundali, aarti, puja_list, booking;
+    CardView kundali, aarti, puja_list, booking, whatshapp, playstore, mail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,9 +46,83 @@ public class HomePageVer2Activity extends AppCompatActivity {
         app_infp_detail = findViewById(R.id.detail_1);
         puja_list = findViewById(R.id.cardView2);
         booking = findViewById(R.id.cardView13);
+        whatshapp = findViewById(R.id.whatshapp);
+        playstore = findViewById(R.id.cardView15);
+        mail = findViewById(R.id.cardView18);
 
 
 
+
+
+
+
+
+
+// Mail OnClickListener
+        mail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                triggerVibration();
+                openEmailClient();
+            }
+            // Define the openEmailClient method outside of the onClick method
+            private void openEmailClient() {
+                // Create the intent to open the email client
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.setType("message/rfc822"); // Set MIME type to target email clients
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"abhijeetpandeydhn@gmail.com"}); // Replace with the desired email address
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Inquiry from Ganpati App User"); // Set the email subject
+                emailIntent.putExtra(Intent.EXTRA_TEXT,
+                        "Hey,\n\n" +
+                                "I am a Ganpati App user, and I wanted to get in touch regarding \n\n" +
+                                "[your query or concern]. \n\n " +
+                                "Thank you!\n\n" +
+                                "Best regards,\n" +
+                                "[Your Name]"); // Set the email body text
+
+                // Try to open an email app, and handle the case where no app is found
+                try {
+                    startActivity(Intent.createChooser(emailIntent, "Choose an email client"));
+                } catch (android.content.ActivityNotFoundException ex) {
+                    // Handle the case when no email app is available
+                    Toast.makeText(HomePageVer2Activity.this, "No email app found", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+
+
+
+
+
+
+
+//playstore
+        playstore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotourl("https://play.google.com/store/apps/details?id=com.abhijeet.ganpatiapp");
+                triggerVibration();
+            }
+            public void gotourl(String s){
+                Uri uri = Uri.parse(s);
+                startActivity(new Intent(Intent.ACTION_VIEW, uri));
+            }
+        });
+
+
+//whatshapp
+        whatshapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotourl("https://wa.me/+919798554810");
+                triggerVibration();
+            }
+            public void gotourl(String s){
+                Uri uri = Uri.parse(s);
+                startActivity(new Intent(Intent.ACTION_VIEW, uri));
+            }
+        });
 
 
 // Booking OnClickListener
