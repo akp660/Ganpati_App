bddc304f9e885919ad5ac77cae843471077ab2a4
abhijeet <abhijeetpandey.dhn@gmail.com>
2024-10-01 17:00:56 +0530

    updated

--- app/src/main/java/com/abhijeet/ganpatiapp/activities/HomePageVer2Activity.java
+++ app/src/main/java/com/abhijeet/ganpatiapp/activities/HomePageVer2Activity.java
@@ -239,4 +239,12 @@ public class HomePageVer2Activity extends AppCompatActivity {
         }
     }
 
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
+    }
+
 }
