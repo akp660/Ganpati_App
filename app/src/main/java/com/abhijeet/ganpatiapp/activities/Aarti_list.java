19ac15ed9872b970d05d4eeaec1081a5307c1df5
akp660 <abhijeetpandey.dhn@gmail.com>
2024-10-10 21:11:10 +0530

    updated

--- app/src/main/java/com/abhijeet/ganpatiapp/activities/Aarti_list.java
+++ app/src/main/java/com/abhijeet/ganpatiapp/activities/Aarti_list.java
@@ -6,15 +6,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.VibrationEffect;
-import android.os.Vibrator;
 import android.util.Log;
-
 import com.abhijeet.ganpatiapp.R;
 import com.abhijeet.ganpatiapp.adapters.Aarti_List_Adapter;
 import com.abhijeet.ganpatiapp.modelclass.Aarti_List_Model_Class;
@@ -23,7 +17,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
 import java.util.List;
 
