1dbca64d3a6317e093bb8aa97bf9f5e0a155ac29
akp660 <abhijeetpandey.dhn@gmail.com>
2024-11-24 14:13:56 +0530

    updated all page ui upgraded to have full page UI.

--- app/src/main/java/com/abhijeet/ganpatiapp/activities/Splash_screen.java
+++ app/src/main/java/com/abhijeet/ganpatiapp/activities/Splash_screen.java
@@ -1,6 +1,10 @@
 package com.abhijeet.ganpatiapp.activities;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,7 +17,14 @@ public class Splash_screen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash_screen);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.imageView12), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
 
         new Handler().postDelayed(new Runnable() {
